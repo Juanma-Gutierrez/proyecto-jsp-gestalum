12c5ef9f508160a6dabe87a7ff8ff9362bfb7d12
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2023-03-17 19:45:38 +0100

    fix: Limpiando temporales del repositorio

--- target/alumnos/App.java
+++
@@ -1,9 +0,0 @@
-import aplicacion.Aplicacion;
-
-public class App {
-
-	public static void main(String[] args) throws Exception {
-		Aplicacion app = new Aplicacion();
-		app.iniciaAplicacion();
-	}
-}
